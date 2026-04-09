66cb6ca6c352711dcf8227356cae2ef2d97c13eb
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-04-09 15:50:45 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ServicesComponent } from './components/services/services.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NgOptimizedImage } from '@angular/common';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports:
+    [
+      RouterOutlet,
+      ServicesComponent,
+      HeroComponent,
+      HeaderComponent,
+      NgOptimizedImage,
+      AboutComponent,
+      PortfolioComponent,
+       ContactComponent,
+       FooterComponent
+    ]
+  ,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/services/services.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+@Component({
+
+  selector: 'app-services',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './services.component.html',
+  styleUrl: './services.component.scss'
+})
+export class ServicesComponent {
+
+}
